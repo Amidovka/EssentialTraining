a5f5c5d5a874da71b26d5880e0141f9b8027dc88
Amidovka <amidovka@gmail.com>
2022-03-22 22:08:44 +0100

    First commit

---
+++ EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SimpleArray();
+            Assert.AreEqual(testInstance.GroceryList.Length, 4, "I expect length to be 4");
+            Assert.AreEqual(testInstance.GroceryList[1], "Milk");
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SimpleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
